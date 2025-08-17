fe2b3024da4434007c908bc11b19144d7a2f1e78
tristan-greffe <tristan.greffe@hotmail.com>
2025-08-17 06:04:34 +0200

    feat: enable image zoom support with vitepress-plugin-image-viewer

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,10 +1,19 @@
 import DefaultTheme from 'vitepress/theme'
 import type { Theme } from 'vitepress'
+import { useRoute } from 'vitepress'
+import 'viewerjs/dist/viewer.min.css'
+import imageViewer from 'vitepress-plugin-image-viewer'
+import vImageViewer from 'vitepress-plugin-image-viewer/lib/vImageViewer.vue'
 import './custom.css'
 
 export default {
   extends: DefaultTheme,
   enhanceApp({ app }) {
-
+    app.component('vImageViewer', vImageViewer)
+  },
+  setup() {
+    const route = useRoute()
+    // Enable the plugin
+    imageViewer(route)
   }
 } satisfies Theme
