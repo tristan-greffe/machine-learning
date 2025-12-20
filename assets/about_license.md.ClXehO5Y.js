90572ff911976ff87f310079db5a9989e76c45f7
tristan-greffe <tristan-greffe@users.noreply.github.com>
2025-12-20 23:22:08 +0000

    Deploying to gh-pages from @ tristan-greffe/machine-learning@135c314461e3ab73793fcddc5d67ea9ee9aea574 🚀

---
+++ assets/about_license.md.ClXehO5Y.js
@@ -0,0 +1,21 @@
+import{_ as n,c as a,o as e,a0 as p}from"./chunks/framework.LSfXfePu.js";const I=JSON.parse('{"title":"Licence","description":"","frontmatter":{},"headers":[],"relativePath":"about/license.md","filePath":"about/license.md"}'),i={name:"about/license.md"};function t(o,s,l,c,r,T){return e(),a("div",null,[...s[0]||(s[0]=[p(`<h1 id="licence" tabindex="-1">Licence <a class="header-anchor" href="#licence" aria-label="Permalink to &quot;Licence&quot;">​</a></h1><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>MIT License</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>Copyright (c) 2023 Tristan Greffe</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>Permission is hereby granted, free of charge, to any person obtaining a copy</span></span>
+<span class="line"><span>of this software and associated documentation files (the &quot;Software&quot;), to deal</span></span>
+<span class="line"><span>in the Software without restriction, including without limitation the rights</span></span>
+<span class="line"><span>to use, copy, modify, merge, publish, distribute, sublicense, and/or sell</span></span>
+<span class="line"><span>copies of the Software, and to permit persons to whom the Software is</span></span>
+<span class="line"><span>furnished to do so, subject to the following conditions:</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>The above copyright notice and this permission notice shall be included in all</span></span>
+<span class="line"><span>copies or substantial portions of the Software.</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>THE SOFTWARE IS PROVIDED &quot;AS IS&quot;, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR</span></span>
+<span class="line"><span>IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,</span></span>
+<span class="line"><span>FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE</span></span>
+<span class="line"><span>AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER</span></span>
+<span class="line"><span>LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,</span></span>
+<span class="line"><span>OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE</span></span>
+<span class="line"><span>SOFTWARE.</span></span></code></pre></div>`,2)])])}const E=n(i,[["render",t]]);export{I as __pageData,E as default};
